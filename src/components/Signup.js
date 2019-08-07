778315b42713e64fe971e24bc2f2050fb3762e8f
Eddie Tapia <etapiahe@ucsd.edu>
2019-08-07 16:25:24 -0700

    added the render method to Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -21,5 +21,97 @@ class Signup extends React.Component {
         };
     }
     //... other class methods
+    render() {
+        if (this.state.redirect) return ;
+        return (
+            <React.Fragment>
+            <Row
+        className='d-flex justify-content-center align-items-center w-100 mt-5'
+        style={{
+            minHeight: '100%'
+        }}
+    >
+    >Col>
+        {this.state.errors !== null && (
+                <Alert variant='danger'>
+            <ul>
+            {this.showErrors().map(err => (
+                    <li key={err}>{err</li>
+    ))}
+    </ul>
+        </Alert>
+    )}
+    <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId='username'>
+            <Form.Label>User ID</Form.Label>
+        <Form.Control
+        required
+        type='text'
+        name='uid'
+        value={this.state.uid}
+        placeholder='Choose a username'
+        onChange={this.handleChange}
+        />
+        {this.state.UIDError !== null && (
+        <Form.Control.Feedback
+            style={{ display: 'block' }}
+            type='invalid'
+                >
+                {this.state.UIDError}
+                </Form.Control.Feedback>
+        )}
+    </Form.Group>
+        <Form.Group controlId='display-name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+        required
+        type='text'
+        name='name'
+        value={this.state.name}
+        placeholder='What is your name?'
+        onChange={this.handleChange}
+        />
+        </Form.Group>
+        <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+        <Form.Control
+        required
+        type='email'
+        name='email'
+        value={this.state.email}
+        placeholder='Your email address'
+        onChange={this.handleChange}
+        />
+        </Form.Group>
+        <Button
+        disabled={this.state.isLoading}
+        variant='primary'
+        type='submit'
+        className='btn-block'
+            >
+            {this.state.isLoading ? (
+                    <>
+                    <Spinner
+                    as='span'
+                    animation='grow'
+                size='sm'
+                role='status'
+                aria-hidden='true'
+                    />
+                    Please wait...
+            </>
+    ) : (
+        <span>Create My Account</span>
+    )}
+    </Button>
+        <p className='pt-3'>
+            Already have an account? <Link to='/'>Login</Link>
+            </p>
+            </Form>
+            </Col>
+            </Row>
+            </React.Fragment>
+    );
+    }
 }
 export default Signup;
